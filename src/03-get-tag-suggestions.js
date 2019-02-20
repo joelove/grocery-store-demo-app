f56d97ca6b1bed082dbe0fb1f54f961541e93d6b
Joe Love <git@joelove.uk>
2019-02-20 12:40:20 +0000

    get tag suggestions

--- src/03-get-tag-suggestions.js
+++ src/03-get-tag-suggestions.js
@@ -1,14 +1,18 @@
-import _ from 'lodash'
-import products from './data/products.json'
+import axios from 'axios'
 
 export function getTagSuggestions(productName) {
-  const matchingProducts = _.filter(products, product => {
-    return product.name.toLowerCase().includes(productName.toLowerCase())
+  return axios.post('https://aito-grocery-store.api.aito.ai/api/v1/_predict', {
+    from: 'products',
+    where: {
+      name: productName
+    },
+    predict: 'tags',
+    exclusiveness: false,
+    limit: 3
+  }, {
+    headers: { 'x-api-key': process.env.REACT_APP_API_KEY },
   })
-
-  // We could sort the tags based on the occurance, but for now we'll just use the first
-  // matching ones
-  const tags = _.uniq(_.flatten(_.map(matchingProducts, product => product.tags.split(' '))))
-
-  return _.take(tags, 3)
+    .then(response => {
+      return response.data.hits.map(hit => hit.feature)
+    })
 }
