0e40e82935ff1674abd90c12c80a1f845dc30cad
Joe Love <git@joelove.uk>
2019-02-20 11:23:34 +0000

    add aito api call to retrieve recommended products on search page

--- src/01-search.js
+++ src/01-search.js
@@ -1,16 +1,19 @@
-import _ from 'lodash'
-import products from './data/products.json'
+import axios from 'axios'
 
-// Simple string based search from products
 export function getProductSearchResults(userId, inputValue) {
-  // We could search from user's purchase history, but it was omitted in this demo
-  if (!inputValue) {
-    return []
-  }
-
-  const filteredProducts = _.filter(products, product => {
-    return product.name.toLowerCase().includes(inputValue.toLowerCase())
+  return axios.post('https://aito-grocery-store.api.aito.ai/api/v1/_search', {
+    from: 'products',
+    where: {
+      $or: [
+        { name: { $match: inputValue } },
+        { tags: { $match: inputValue } }
+      ]
+    },
+    limit: 5
+  }, {
+    headers: { 'x-api-key': process.env.API_KEY },
   })
-
-  return _.take(filteredProducts, 5)
+    .then(response => {
+      return response.data.hits
+    })
 }
